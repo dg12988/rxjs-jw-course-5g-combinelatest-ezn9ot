e8afe9a0dd2f7139166b6285cc79037c4f55a069
dg12988 <doug.goldberg@me.com>
2021-08-10 03:27:52 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -9,6 +9,18 @@ const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
 
 combineLatest([temperatureInputEvent$, conversionInputEvent$]).subscribe(
   ([temperatureInputEvent, conversionInputEvent]) => {
+    const temperature =  Number(temperatureInputEvent.target['value']);
+    const conversion = conversionInputEvent.target['value'];
+
+    let result: number;
+    if(conversion === 'f-to-c'){
+      result = (temperature - 32) * 5/9;  
+    } else if (conversion === 'c-to-f'){
+      result = temperature * 9/5 + 32;
+    }
+
+    resultText.innerText = String(result);
+
     console.log(
       temperatureInputEvent.target['value'],
       conversionInputEvent.target['value']
