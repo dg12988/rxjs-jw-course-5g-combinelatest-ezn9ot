52bb74fffe120cd3f9f108f1beb517830d0956fd
dg12988 <doug.goldberg@me.com>
2021-08-10 03:17:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-5g-combinelatest-ezn9ot
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-5g-combinelatest-ezn9ot)
---
+++ index.ts
@@ -0,0 +1,4 @@
+const temperatureInput = document.getElementById('temperature-input');
+const conversionDropdown = document.getElementById('conversion-dropdown');
+const resultText = document.getElementById('result-text');
+
