abafc396c06655affd601e39800c19d43e75cc2e
dg12988 <doug.goldberg@me.com>
2021-08-10 03:21:54 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
+import { combineLatest, fromEvent } from "rxjs";
+
 const temperatureInput = document.getElementById('temperature-input');
 const conversionDropdown = document.getElementById('conversion-dropdown');
 const resultText = document.getElementById('result-text');
+
+const temperatureInputEvent$ = fromEvent(temperatureInput, 'input');
+const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
+
+combineLatest([temperatureInputEvent$, conversionInputEvent$]).subscribe(
+  ([temperatureInputEvent, conversionInputEvent]) => {
+    console.log(
+      temperatureInputEvent.target['value'],
+      conversionInputEvent.target['value']
+    );
+  }
+);
 
